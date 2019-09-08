7818a41200a2a4928f6f782a40d9befb052f76cb
Natalya Norboeva <divonata@gmail.com>
2019-09-08 19:14:02 +0700

    create, edit category; search and select parent category

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -3,19 +3,52 @@
     <td>
       <input class="category-edit__input" v-model="title" :disabled="!isEditing" type="text">
     </td>
-    <td class="category-edit__parent">{{ category.parent }}</td>
+    <td class="category-edit__parent">
+      <SearchSelect
+        v-if="isEditing"
+        :searching-value="parentTitle"
+        searching-prop="title"
+        :options="parents"
+        :required="false"
+        placeholder="Название"
+        @input="onInputParent"
+        @blur="onBlur"
+        @select="onSelectParent"
+      >
+        <template v-slot:default="slotProps">
+          <div>
+            {{ slotProps.option.title }}
+          </div>
+        </template>
+      </SearchSelect>
+      <div v-else-if="parent">{{ parent.title }}</div>
+      <div v-if="error" class="error">{{ error }}</div>
+    </td>
     <td class="category-edit__children">{{ category.children.size }}</td>
     <td class="category-edit__messages">{{ category.messages.size }}</td>
     <td>
-      <button class="category-edit__save" v-if="isEditing" type="button" @click.stop="onSave">
-        save
+      <template v-if="isEditing">
+        <button class="category-edit__save"  type="button" @click.stop="onSave">
+          Сохранить
+        </button>
+        <button class="category-edit__save"  type="button" @click.stop="onCancel">
+          Отмена
+        </button>
+      </template>
+      <button class="category-edit__save" v-else type="button" @click.stop="onEdit">
+        Изменить
       </button>
     </td>
   </tr>
 </template>
 
 <script>
+import { debounce } from 'lodash';
+import SearchSelect from '@/components/SearchSelect.vue';
+import Category from '@/classes/Category';
+
 export default {
+  components: { SearchSelect },
   props: {
     category: {
       type: Object,
@@ -32,35 +65,78 @@ export default {
   },
   data() {
     return {
+      error: '',
       id: '',
       title: '',
-      parent: {},
+      parent: null,
+      parents: [],
+      parentTitle: '',
       children: [],
       messages: [],
     };
   },
   created() {
-    const {
-      id, title, parent, children, messages,
-    } = this.category;
-    this.id = id;
-    this.title = title;
-    this.parent = parent;
-    this.children = children;
-    this.messages = messages;
+    this.init();
+
+    this.debouncedsearchParent = debounce(this.searchParent, 300);
   },
   methods: {
+    init() {
+      const {
+        id, title, parent, children, messages,
+      } = this.category;
+      this.id = id;
+      this.title = title;
+      this.children = children;
+      this.messages = messages;
+
+      if (parent) {
+        Category.getById({ id: parent.id }).then((res) => {
+          this.parent = new Category(res);
+        });
+      }
+    },
+    onBlur(e) {
+      if (e.target && e.target.value && !this.parent) {
+        this.error = 'Выберите категорию из предложенных';
+      }
+    },
+    onCancel() {
+      this.$emit('cancel');
+    },
+    onEdit() {
+      this.$emit('edit', this.category.id);
+    },
+    onInputParent(val) {
+      this.parentTitle = val;
+      this.debouncedsearchParent(val);
+    },
     onSelect() {
       if (!this.isEditing) {
         this.$emit('select', this.category.id);
       }
+    },
+    onSelectParent(category) {
+      this.error = '';
+      this.parentTitle = category.title;
+      this.parent = category;
     },
     onSave() {
       const {
         id, title, parent, children, messages,
       } = this;
       this.$emit('save', {
-        id, title, parent, children, messages,
+        id,
+        title,
+        children,
+        messages,
+        parent: parent ? { id: parent.id } : null,
+      });
+    },
+    searchParent(title) {
+      const trimmed = title.trim();
+      return Category.getByTitle({ title: trimmed }).then((res) => {
+        this.parents = res;
       });
     },
   },
@@ -78,6 +154,10 @@ export default {
   }
   &__input {
     background-color:  #fff;
+  }
+  .error {
+    font-size: 12px;
+    color: red;
   }
 }
 </style>
--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -1,20 +1,32 @@
 <template>
-  <table class="categories">
-    <tr class="categories__head">
-      <td>Название</td>
-      <td>Родительская категория</td>
-      <td>Подкатегории</td>
-      <td colspan="2">Сообщения</td>
-    </tr>
-    <tr is="CategoryEdit"
-        v-for="category in categories"
-        :key="category.id"
-        :is-editing="false"
-        :category="category"
-        @select="showCategory"
-        @save="saveCategory"
-    />
-  </table>
+  <div class="categories">
+    <button type="button" @click="createCategory">Создать категорию</button>
+    <table class="categories__table">
+      <tr class="categories__head">
+        <td>Название</td>
+        <td>Родительская категория</td>
+        <td>Подкатегории</td>
+        <td colspan="2">Сообщения</td>
+      </tr>
+      <tr v-if="isCreating"
+          is="CategoryEdit"
+          :is-editing="isCreating"
+          :category="createdCategory"
+          @cancel="isCreating = false"
+          @save="saveCategory(createdCategory, $event)"
+      />
+      <tr is="CategoryEdit"
+          v-for="category in categories"
+          :key="category.id"
+          :is-editing="category.id === editingCategoryId"
+          :category="category"
+          @cancel="editingCategoryId = 0"
+          @edit="editingCategoryId = $event"
+          @save="saveCategory(category, $event)"
+          @select="showCategory"
+      />
+    </table>
+  </div>
 </template>
 
 <script>
@@ -29,8 +41,10 @@ export default {
   },
   data() {
     return {
-      category: new Category(),
+      createdCategory: new Category(),
       categories: [],
+      isCreating: false,
+      editingCategoryId: 0,
     };
   },
   created() {
@@ -40,20 +54,34 @@ export default {
     ...mapActions([
       'setCategory',
     ]),
+    createCategory() {
+      this.isCreating = true;
+    },
     getCategories() {
-      Category.getSome()
+      Category.getSome({ perPage: 15 })
         .then((res) => {
           this.categories = res.data.map(d => new Category(d));
         });
     },
-    saveCategory(data) {
-      this.category.create(data)
+    saveCategory(category, data) {
+      if (category.id) {
+        category.update(data)
+          .then(() => {
+            this.editingCategoryId = 0;
+          });
+        return;
+      }
+      category.create(data)
         .then((res) => {
           const primaryKey = res['Primary key'];
           this.category.id = primaryKey && primaryKey.id;
+          this.isCreating = false;
         });
     },
     showCategory(id) {
+      if (this.editingCategoryId) {
+        return;
+      }
       const cat = this.categories.find(c => c.id === id);
       this.setCategory(cat);
       this.$router.push({ name: 'category', params: { id } });
@@ -64,7 +92,9 @@ export default {
 
 <style lang="less" scoped>
 .categories {
-  border-collapse: collapse;
+  &__table {
+    border-collapse: collapse;
+  }
   &__head {
     color: #2c3e50;
     td {
