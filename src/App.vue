2c38be82f3315a1fa8b1abc97a5b48ffea500915
Natalya Norboeva <divonata@gmail.com>
2019-09-10 13:08:59 +0700

    App: common input and button styles

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,35 @@ export default {
 </script>
 
 <style lang="less">
+ @import "assets/styles/colors";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: @text-color;
   * {
     box-sizing: border-box;
   }
   input {
     border: none;
+    outline: none;
+    padding: 4px;
+    color: @text-color;
+    &::placeholder {
+      color: rgba(7, 16, 28, 0.6);
+    }
+  }
+  button {
+    border: 1px solid rgba(7, 16, 28, 0.3);
+    outline: none;
+    padding: 4px;
+    background-color: #fff;
+    color: @text-color;
+    &[disabled] {
+      background-color: rgba(7, 16, 28, 0.1);
+      color: rgba(7, 16, 28, 0.6);
+    }
   }
 }
 </style>
