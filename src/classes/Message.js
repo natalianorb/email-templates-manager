863fc3b1fc8bd7e51c1ee76e548e345eb351fb0b
Natalya Norboeva <divonata@gmail.com>
2019-09-12 21:39:40 +0700

    Message class: added methods

--- src/classes/Message.js
+++ src/classes/Message.js
@@ -3,17 +3,46 @@ import Base from '@/classes/Base';
 import { baseUrl } from '@/env';
 
 class Message extends Base {
-  constructor(props) {
+  constructor(props = {
+    id: '', title: '', body: '', category: '',
+  }) {
     super(props);
     this.body = props.body || '';
     this.category = props.category;
   }
 
-  get(options = { perPage: 10 }) {
+  // eslint-disable-next-line class-methods-use-this
+  create(data) {
+    return axios
+      .post(baseUrl, {
+        jsonrpc: '2.0',
+        method: 'createMessage',
+        id: 'test',
+        params: {
+          data,
+        },
+      })
+      .then(res => res.data && res.data.result);
+  }
+
+  delete() {
+    return axios
+      .post(baseUrl, {
+        jsonrpc: '2.0',
+        method: 'deleteMessage',
+        id: 'test',
+        params: {
+          conditions: ['id', '=', this.id],
+        },
+      })
+      .then(res => res.data && res.data.result);
+  }
+
+  getById(options = { perPage: 10 }) {
     const { id } = this;
 
     if (!id) {
-      return Promise.reject(new Error('get Category requires id'));
+      return Promise.reject(new Error('get Message requires id'));
     }
     return axios
       .post(baseUrl, {
@@ -24,6 +53,59 @@ class Message extends Base {
           conditions: ['id', '=', id],
           page: options.page,
           perPage: options.perPage,
+        },
+      })
+      .then(res => res.data && res.data.result);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  update(data) {
+    const { id } = data;
+
+    if (!id) {
+      return Promise.reject(new Error('save requires id'));
+    }
+    return axios
+      .post(baseUrl, {
+        jsonrpc: '2.0',
+        method: 'updateMessage',
+        id: 'test',
+        params: {
+          conditions: ['id', '=', data.id],
+          data,
+        },
+      })
+      .then(res => res.data && res.data.result);
+  }
+
+  static get({ perPage = 10, page, conditions = ['id', 'IS NOT NULL'] }) {
+    return axios
+      .post(baseUrl, {
+        jsonrpc: '2.0',
+        method: 'readMessage',
+        id: 'test',
+        params: {
+          conditions,
+          page,
+          perPage,
+        },
+      })
+      .then(res => res.data && res.data.result);
+  }
+
+  static getMessagesOf({ perPage = 10, page, categoryId }) {
+    if (!categoryId) {
+      return Promise.reject(new Error('getMessagesOf requires Category id'));
+    }
+    return axios
+      .post(baseUrl, {
+        jsonrpc: '2.0',
+        method: 'readMessage',
+        id: 'test',
+        params: {
+          conditions: ['category', '=', categoryId],
+          page,
+          perPage,
         },
       })
       .then(res => res.data && res.data.result);
