48fc4a8d1d1625e53524a3cd639395c043214b57
Natalya Norboeva <divonata@gmail.com>
2019-09-16 12:45:23 +0700

    added Vuelidate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import store from './store';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   store,
